9d8a1125c5ddbd244ebffd784e0908f982fcb748
yasher3413 <yashgandhi2023@gmail.com>
2025-03-31 13:31:09 -0400

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,7 +81,7 @@ export default function Home() {
           <Terminal />
           <Hero />
         </section>
-        <div className="container mx-auto px-4 space-y-24">
+        <div className="container mx-auto px-4 space-y-4">
           <section id="about">
             <About />
           </section>
